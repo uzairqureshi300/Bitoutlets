e5ca271b3bbb714be918526d8e1cfbb134832dd6
uzair <uzair.qureshi@silverback.com.pk>
2017-04-27 23:24:05 +0500

    ourwallet(Login API done)

--- app/src/main/java/ourwallet/example/com/ourwallet/MainActivity.java
+++ app/src/main/java/ourwallet/example/com/ourwallet/MainActivity.java
@@ -2,6 +2,7 @@ package ourwallet.example.com.ourwallet;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button login;
     private EditText email,password;
     private ProgressDialog mProgressDialog;
+    private View snack_bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         login=(Button)findViewById(R.id.login);
         email=(EditText)findViewById(R.id.input_email);
         password=(EditText)findViewById(R.id.input_password);
+        snack_bar=(View)findViewById(R.id.snackbar_view);
         login.setOnClickListener(this);
     }
 
@@ -39,7 +43,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         view.getId();
         switch (view.getId()){
             case R.id.login:
-                Login();
+                try {
+                    Login();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
         }
     }
@@ -47,14 +55,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    private void Login() {
+    private void Login() throws JSONException {
       showProgressDialog();
-        Map<String,String> params = new HashMap<String, String>();
-        params.put("username", "tierra");
-        params.put("password", "Partners5$#21");
+        JSONObject json = new JSONObject();
+        json.put("username", email.getText().toString());
+        json.put("password", password.getText().toString());
 
-        String url = "http://orupartners.com/cp/admin/login/log_in";
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, url,new JSONObject(params), this ,this){
+        JSONObject json2 = new JSONObject();
+        json2.put("to","orupartners");
+        json2.put("methods","log_in");
+        json2.accumulate("complex",json);
+        String url = "http://orupartners.com/cp/redirect_to.php";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, url,json2, this ,this){
 
         };
         jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(
@@ -64,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
     }
     private void showProgressDialog() {
-        mProgressDialog = new ProgressDialog(MainActivity.this);
+        mProgressDialog = new ProgressDialog(MainActivity.this,R.style.AppCompatAlertDialogStyle);
         mProgressDialog.setMessage("Please Wait..");
         mProgressDialog.setCancelable(false);
         mProgressDialog.show();
@@ -80,8 +92,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.e("RESPONSE",response.toString());
         try {
             String error = response.getString("error");
-            String msg=response.getString("msg");
-            Log.e("error"  ,error+"     "+ msg);
+            if(error.equals("0")){
+                Intent i=new Intent(getApplicationContext(),Home.class);
+                startActivity(i);
+                finish();
+            }
+            else{
+                Snackbar.make(snack_bar,"Invalid Credentials",Snackbar.LENGTH_LONG).show();
+            }
             mProgressDialog.dismiss();
         }
         catch(Exception ex){
